ebd4ce7f889a934fa5d8fd323983751bd9ebc49e
Alyrra-Codes <ahodsden3@gmail.com>
2024-09-30 16:57:29 +1000

    Further progress made on time selection page. Some tweaks to the other pages too

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,41 @@
+var selectFlag = false
+
 function goToPage(id){
     console.log('Navigate ' + id);
     window.location = 'select-time.html';
+}
+
+function handleSelectTime(id){
+    e = document.getElementById(id)
+    if(selectFlag == false){
+        e.classList.add('selected')
+        e.id = 'selected'
+        selectFlag = true
+    }
+    else{
+        if(id == 'selected'){   
+            // remove class from previous selected time     
+            s = document.getElementById('selected')
+            s.classList.remove('selected')
+            s.id = s.innerHTML
+            selectFlag = false
+        }
+        else{
+            // remove class from previous selected time
+            s = document.getElementById('selected')
+            s.classList.remove('selected')
+            s.id = s.innerHTML
+            // add class to new selection
+            e.classList.add('selected')
+            e.id = 'selected'
+        }
+    }
+
+}
+
+function submitTime() {
+    time = document.getElementById('selected').innerHTML
+    if(time == "09:00"){
+        console.log(time) 
+    }
 }
